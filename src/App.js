f5b16edfa951221f561722a9aca671e4b97943b5
dbobb220 <david.bobb.220@gmail.com>
2019-06-04 10:18:22 -0500

    setup api fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoading: true,
+      beerList: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch('https://api.punkapi.com/v2/beers')
+      .then(res => res.json())
+      .then(data => data.map(beer => ({
+        name: beer.name,
+        id: beer.id,
+        tagline: beer.tagline,
+        image: beer.image_url
+      })))
+      .then(beerList => this.setState({
+        beerList,
+        isLoading: false,
+      }))
+      .catch(err => console.log('There was an error', err))
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <App/>
+      </div>
+    )
+  }
 }
 
 export default App;
